a1a5d8fd60c46d841203abdb7d98aae8b063c151
Dmitri Maksimov <dmitri.maksimov@bytelife.com>
2016-10-05 20:26:15 +0300

    Added enumeration for 'picture-options' of gsettings 'org.mate.backgorund' schema.

--- src/main/kotlin/ee/mcdimus/matewp/service/OperationSystemService.kt
+++ src/main/kotlin/ee/mcdimus/matewp/service/OperationSystemService.kt
@@ -15,16 +15,29 @@ class OperationSystemService {
     private const val SET_CMD = "set"
     private const val GET_CMD = "get"
     private const val SCHEMA = "org.mate.background"
-    private const val KEY = "picture-filename"
+    private const val KEY_PICTURE_FILENAME = "picture-filename"
+    private const val KEY_PICTURE_OPTIONS = "picture-options"
+
+    enum class PictureOptions {
+      WALLPAPER,
+      CENTERED,
+      SCALED,
+      STRETCHED,
+      ZOOM,
+      SPANNED;
+
+      override fun toString() = name.toLowerCase()
+    }
   }
 
   fun setAsWallpaper(filePath: Path) {
     // execute command 'gsettings set org.mate.background picture-filename '/home/dmitri/Pictures/mate-wp/___.jpg''
-    execCommand(GSETTINGS, SET_CMD, SCHEMA, KEY, "'${filePath.toAbsolutePath()}'")
+    execCommand(GSETTINGS, SET_CMD, SCHEMA, KEY_PICTURE_FILENAME, "'${filePath.toAbsolutePath()}'")
+    execCommand(GSETTINGS, SET_CMD, SCHEMA, KEY_PICTURE_OPTIONS, "'${PictureOptions.STRETCHED}'")
   }
 
   fun getCurrentWallpaper(): Path {
-    return Paths.get(execCommand(GSETTINGS, GET_CMD, SCHEMA, KEY)!!.trim('\'')).toAbsolutePath()
+    return Paths.get(execCommand(GSETTINGS, GET_CMD, SCHEMA, KEY_PICTURE_FILENAME)!!.trim('\'')).toAbsolutePath()
   }
 
   private fun execCommand(vararg args: String): String? {
