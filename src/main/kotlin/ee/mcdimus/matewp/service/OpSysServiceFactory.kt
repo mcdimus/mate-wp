aec27b86aec0bf8560bf7c689e9d074974f98e63
Dmitri Maksimov <dmitri.maksimov@bytelife.com>
2016-10-11 15:29:38 +0300

    Improved OS detection in OpSysSystemFactory.

--- src/main/kotlin/ee/mcdimus/matewp/service/OpSysServiceFactory.kt
+++ src/main/kotlin/ee/mcdimus/matewp/service/OpSysServiceFactory.kt
@@ -5,18 +5,34 @@ package ee.mcdimus.matewp.service
  */
 object OpSysServiceFactory {
 
-  enum class OS {
+  private enum class OS {
     LINUX, WINDOWS
   }
 
-  fun get(): OpSysService = when (detectOS()) {
-    OS.LINUX -> LinuxMateService()
-    OS.WINDOWS -> WindowsService()
+  fun get(): OpSysService {
+    return when (detectOS()) {
+      OS.LINUX -> LinuxMateService()
+      OS.WINDOWS -> WindowsService()
+    }
   }
 
-  private fun detectOS() = when (System.getProperty("os.name")) {
-    "Windows 7" -> OS.WINDOWS
-    else -> OS.LINUX
+  private fun detectOS(): OS {
+    val osName = System.getProperty("os.name")
+    return when (osName) {
+      "Windows 7" -> OS.WINDOWS
+      "Linux" -> OS.LINUX
+      else -> makeGuess(osName)
+    }
+  }
+
+  private fun  makeGuess(osName: String): OS {
+    if (osName.contains("windows", ignoreCase = true)) {
+      return OS.WINDOWS
+    } else if (osName.contains("linux", ignoreCase = true)) {
+      return OS.LINUX
+    } else {
+      throw IllegalStateException("unsupported operating system: $osName")
+    }
   }
 
 }
