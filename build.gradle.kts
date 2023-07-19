8045ebf41a17fa7e3202500340f77a95191aaee1
Dmitri Maksimov <mcdimus@gmail.com>
2023-07-19 12:13:48 +0300

    chore: update Gradle wrapper to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jreleaser.gradle.plugin.tasks.JReleaserAssembleTask
 import org.jreleaser.model.Active.ALWAYS
@@ -21,7 +19,7 @@ group = "eu.maksimov"
 version = "1.0.0"
 
 application {
-  mainClass.set("ee.mcdimus.matewp.MainKt")
+  mainClass = "ee.mcdimus.matewp.MainKt"
 }
 
 repositories {
@@ -30,7 +28,7 @@ repositories {
 
 java {
   toolchain {
-    languageVersion.set(JavaLanguageVersion.of(17))
+    languageVersion = JavaLanguageVersion.of(17)
   }
 }
 
@@ -61,7 +59,7 @@ dependencies {
 }
 
 versionCatalogUpdate {
-  sortByKey.set(false)
+  sortByKey = false
 }
 
 tasks.withType<Test>().configureEach {
@@ -82,13 +80,13 @@ tasks.withType<JReleaserAssembleTask> {
 
 jreleaser {
   project {
-    vendor.set("Dmitri Maksimov")
-    authors.set(listOf("Dmitri Maksimov"))
-    description.set("Photo Of the Day from Bing")
-    license.set("Apache-2.0")
-    inceptionYear.set("2014")
+    vendor = "Dmitri Maksimov"
+    authors = listOf("Dmitri Maksimov")
+    description = "Photo Of the Day from Bing"
+    license = "Apache-2.0"
+    inceptionYear = "2014"
     links {
-      homepage.set("https://github.com/mcdimus/mate-wp")
+      homepage = "https://github.com/mcdimus/mate-wp"
     }
     java {
 
@@ -97,101 +95,106 @@ jreleaser {
   assemble {
     jlink {
       create("app-jlink") {
-        active.set(ALWAYS)
-        exported.set(false)
-        executable.set("mate-wp") // Name of the executable launcher. If left undefined, will use ${assembler.name}.
+        active = ALWAYS
+        exported = false
+        executable = "mate-wp" // Name of the executable launcher. If left undefined, will use ${assembler.name}.
 
         val compiler = javaToolchains.compilerFor {
-          languageVersion.set(JavaLanguageVersion.of(17))
+          languageVersion = JavaLanguageVersion.of(17)
         }
 
         // List of JDKs for generating cross-platform images.
         targetJdk {
-          platform.set("linux-x86_64")
-          path.set(compiler.get().metadata.installationPath.asFile)
+          platform = "linux-x86_64"
+          path = compiler.get().metadata.installationPath.asFile
         }
 
         // The JDK to use. If undefined, will find a matching JDK.
         jdk {
-          platform.set("linux-x86_64")
-          path.set(compiler.get().metadata.installationPath.asFile)
+          platform = "linux-x86_64"
+          path = compiler.get().metadata.installationPath.asFile
         }
 
         jdeps {
-          ignoreMissingDeps.set(false)
-          useWildcardInPath.set(true)
-          multiRelease.set("17")
+          ignoreMissingDeps = false
+          useWildcardInPath = true
+          multiRelease = "17"
           // JARs or classes used to limit the module search. Must be defined as paths. Defaults to empty.
           targets.add(File(projectDir, "build/classes/kotlin/main/ee/mcdimus/matewp/MainKt.class").absolutePath)
         }
         additionalModuleNames.add("jdk.crypto.ec") // required for SSL
         additionalModuleNames.add("java.naming") // required for logback
 
-        copyJars.set(true)        // Copy main and input JARs into archive. Defaults to `true`.
+        copyJars = true        // Copy main and input JARs into archive. Defaults to `true`.
 
         // The executable JAR that contains the application.
         mainJar {
-          path.set(File("build/libs/mate-wp-{{projectVersion}}.jar"))
+          path = File("build/libs/mate-wp-{{projectVersion}}.jar")
         }
 
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.forEach {
           jars {
             setDirectory(it.parent)
-            pattern.set("*.jar")
+            pattern = "*.jar"
           }
         }
 
         java {
-//            groupId.set("${project.java.groupId}")           // Maven coordinates = groupId. If left undefined, will use ${project.java.groupId}.
-//            artifactId.set("${project.java.artifactId}")           // Maven coordinates = artifactId. If left undefined, will use ${project.java.artifactId}.
-          version.set("17")      // The minimum Java version required by consumers to run the application. If left undefined, will use ${project.java.version}
-          mainClass.set("ee.mcdimus.matewp.MainKt")       // The application's entry point. If left undefined, will use ${project.java.mainClass}.
-          multiProject.set(false)           // Identifies the project as being member of a multi-project build.  If left undefined, will use ${project.java.multiProject}.
+//        groupId.set("${project.java.groupId}")        // Maven coordinates = groupId. If left undefined, will use ${project.java.groupId}.
+//        artifactId.set("${project.java.artifactId}")  // Maven coordinates = artifactId. If left undefined, will use ${project.java.artifactId}.
+          version =
+            "17"                                // The minimum Java version required by consumers to run the application. If left undefined, will use ${project.java.version}
+          mainClass =
+            "ee.mcdimus.matewp.MainKt"        // The application's entry point. If left undefined, will use ${project.java.mainClass}.
+          multiProject =
+            false                          // Identifies the project as being member of a multi-project build.  If left undefined, will use ${project.java.multiProject}.
         }
       }
     }
 
     jpackage {
       create("app") {
-        active.set(ALWAYS)
-        exported.set(true)
-        verbose.set(true)
-        jlink.set("app-jlink")
-        attachPlatform.set(true)
+        active = ALWAYS
+        exported = true
+        verbose = true
+        jlink = "app-jlink"
+        attachPlatform = true
 
         applicationPackage {
-          appName.set("mate-wp")     // Name of the application and/or package.  If left undefined, will use `${assembler.name}`.
-          vendor.set("me")           // Vendor of the application. If left undefined, will use `${project.vendor}`.
-          copyright.set("Duke 2021") // Copyright for the application. If left undefined, will use `${project.copyright}`.
-          licenseFile.set("path/to/LICENSE")
+          appName =
+            "mate-wp"     // Name of the application and/or package.  If left undefined, will use `${assembler.name}`.
+          vendor = "me"           // Vendor of the application. If left undefined, will use `${project.vendor}`.
+          copyright = "Duke 2021" // Copyright for the application. If left undefined, will use `${project.copyright}`.
+          licenseFile = "path/to/LICENSE"
         }
 
         launcher {
           arguments.add("update")
         }
 
-        //
         linux {
-          installDir.set("/opt/")    // Absolute path of the installation directory of the application.
           types.add("deb")
-          packageName.set("mate-wp") // Name for Linux package. If undefined, will use the assembler's name.
-          maintainer.set("Dmitri Maksimov")       // Maintainer for .deb package.
-          menuGroup.set("apps")      // Menu group this application is placed in.
-          license.set("MIT")         // Type of the license ("License: " of the RPM .spec). If undefined, will use `${project.license}`
-          appRelease.set("1")        // Release value of the RPM .spec file or Debian revision value of the DEB control file.
-          appCategory.set("devel")   // Group value of the RPM .spec file or Section value of DEB control file.
-          shortcut.set(false)        // Creates a shortcut for the application
+          installDir = "/opt/"           // Absolute path of the installation directory of the application.
+          packageName = "mate-wp"        // Name for Linux package. If undefined, will use the assembler's name.
+          maintainer = "Dmitri Maksimov" // Maintainer for .deb package.
+          menuGroup = "apps"             // Menu group this application is placed in.
+          license =
+            "MIT"                // Type of the license ("License: " of the RPM .spec). If undefined, will use `${project.license}`
+          appRelease =
+            "1"               // Release value of the RPM .spec file or Debian revision value of the DEB control file.
+          appCategory = "devel"          // Group value of the RPM .spec file or Section value of DEB control file.
+          shortcut = false               // Creates a shortcut for the application
           jdk {
             val compiler = javaToolchains.compilerFor {
-              languageVersion.set(JavaLanguageVersion.of(17))
+              languageVersion = JavaLanguageVersion.of(17)
             }
-            path.set(compiler.get().metadata.installationPath.asFile)
+            path = compiler.get().metadata.installationPath.asFile
           }
-          resourceDir.set("${projectDir.absolutePath}/etc/jpackage/resources")
+          resourceDir = "${projectDir.absolutePath}/etc/jpackage/resources"
         }
 
         fileSet {
-          input.set("${projectDir.absolutePath}/etc/jpackage/files")
+          input = "${projectDir.absolutePath}/etc/jpackage/files"
         }
       }
     }
@@ -199,6 +202,6 @@ jreleaser {
 }
 
 tasks.wrapper {
-  gradleVersion = "8.1"
+  gradleVersion = "8.2.1"
   distributionType = Wrapper.DistributionType.ALL
 }
